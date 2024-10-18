f5080a8cf70b4836c1ef736937df3433ea95a928
Rishabh Parwal <rishabhparwal149@gmail.com>
2024-10-18 22:52:16 +0530

    added comments

--- car_rental_service.py
+++ car_rental_service.py
@@ -1,7 +1,9 @@
+# Define a class for Vehicle
 class Vehicle:
-    vehicle_type = 'Car'
+    vehicle_type = 'Car'  # Default type of vehicle
 
-    def __init__(self, vehicle_id, make, model, year, rental_rate, availability = True):
+    def __init__(self, vehicle_id, make, model, year, rental_rate, availability=True):
+        # Initialize vehicle attributes
         self.vehicle_id = vehicle_id
         self.make = make
         self.model = model
@@ -10,6 +12,7 @@ class Vehicle:
         self.availability = availability
     
     def print_vehicle_details(self):
+        # Print vehicle details if available
         if self.availability == True:
             print(f"Vehicle ID: {self.vehicle_id}")
             print(f"Vehicle type: {self.vehicle_type}")
@@ -19,48 +22,58 @@ class Vehicle:
             print(f"Rental rate: {self.rental_rate}")
             print(f"Availability: {self.availability}")
 
-            if type(self)==LuxuryVehicle:
+            # Additional details for luxury vehicles
+            if type(self) == LuxuryVehicle:
                 print(f"Sunroof: {self.sunroof}")
                 print(f"Alcantara interior: {self.alcantara}")
         else:
             pass
 
+# Define a class for Luxury Vehicles, inheriting from Vehicle class
 class LuxuryVehicle(Vehicle):
-    def __init__(self, vehicle_id, make, model, year, rental_rate, availability = True, sunroof=True, alcantara=True):
+    def __init__(self, vehicle_id, make, model, year, rental_rate, availability=True, sunroof=True, alcantara=True):
+        # Initialize luxury vehicle attributes
         self.sunroof = sunroof
         self.alcantara = alcantara
-        Vehicle.__init__(self, vehicle_id, make, model, year, rental_rate, availability = True)
-        self.rental_rate = int(self.rental_rate*1.2)
-            
+        Vehicle.__init__(self, vehicle_id, make, model, year, rental_rate, availability=True)
+        self.rental_rate = int(self.rental_rate * 1.2)  # Increase rental rate by 20% for luxury vehicles
+
+# List to store vehicles
 Vehicles = []
+# Adding some vehicles to the list
 Vehicles.append(Vehicle(1, "Honda", "Civic", 2021, 5000, True))
 Vehicles.append(LuxuryVehicle(2, "Skoda", "Superb", 2023, 8000, True))
 
+# List to store customers
 Customers = []
 
-
-
-class Customer():
-    def __init__(self, customer_id, name, contact_info, rental_history = []):
+# Define a class for Customer
+class Customer:
+    def __init__(self, customer_id, name, contact_info, rental_history=[]):
+        # Initialize customer attributes
         self.customer_id = customer_id
         self.name = name
         self.contact_info = contact_info
         self.rental_history = rental_history
 
+    @staticmethod
     def input_customer_details():
-        if input("Are you an existing customer (y/n): ")=="y": existing = True
-        else: existing = False
+        # Input customer details
+        if input("Are you an existing customer (y/n): ") == "y": # Check if customer is existing
+            existing = True
+        else:
+            existing = False
         customer_id = -1
         if existing:
-            existing_id = input("Please enter your ID: ")
+            existing_id = input("Please enter your ID: ") # Input existing customer ID
             for x in Customers:
                 if x.customer_id == existing_id:
                     customer_id = x.customer_id
                     return customer_id
-            if customer_id <0:
-                print("You are not an existing customer. Proceed to create your ID.")
+            if customer_id < 0:
+                print("You are not an existing customer. Proceed to create your ID.") # If customer ID not found
                 print("")
-        customer_id = len(Customers)+1
+        customer_id = len(Customers) + 1
         name = input("Please enter your name: ")
         contact_info = int(input("Please enter your mobile number: "))
         print(f"Your customer ID is: {customer_id}")
@@ -70,71 +83,73 @@ class Customer():
         return customer_id
 
     def print_customer_details(self):
+        # Print customer details
         print(f"Customer ID: {self.customer_id}")
         print(f"Name: {self.name}")
         print(f"Mobile number: {self.contact_info}")
 
-    class rental_info():
-        def __init__(self, vehicle_id, rent_duration, returned = False):
+    # Define a nested class for rental information
+    class rental_info:
+        def __init__(self, vehicle_id, rent_duration, returned=False):
+            # Initialize rental information attributes
             self.vehicle_id = vehicle_id
             self.rent_duration = rent_duration
             self.returned = returned
 
     def print_rental_history(self):
-        i=1
+        # Print customer's rental history
+        i = 1
         for x in self.rental_history:
             print(f"{i} Vehicle ID: {x.vehicle_id}, Duration: {x.rent_duration} days, Returned: {x.returned}")
-            i+=1
+            i += 1
 
-
-
+# Function to rent a vehicle
 def rent_vehicle(Customer):
-    id = int(input("Enter the vehicle ID of the vehicle you want to rent: "))
+    id = int(input("Enter the vehicle ID of the vehicle you want to rent: ")) # Input vehicle ID
     if id <= len(Vehicles):
-        rent_duration = int(input("Enter the number of days you want to rent the vehicle for: "))
-        current_vehicle = Vehicles[id-1]
+        rent_duration = int(input("Enter the number of days you want to rent the vehicle for: ")) # Input rent duration
+        current_vehicle = Vehicles[id - 1]
         if current_vehicle.availability == True:
-            print(f"Vehicle avilable. Total cost for {rent_duration} days will be: {current_vehicle.rental_rate*rent_duration}")
+            print(f"Vehicle available. Total cost for {rent_duration} days will be: {current_vehicle.rental_rate * rent_duration}")
             current_vehicle.availability = False
             Customer.rental_history.append(Customer.rental_info(current_vehicle.vehicle_id, rent_duration, False))
         else:
-            print("The chosen vehicle is not available at the moment. Please choose another vehicle.")
+            print("The chosen vehicle is not available at the moment. Please choose another vehicle.") # If vehicle not available
             rent_vehicle(Customer)
         print("")
-    else: 
-        print("Enter a valid Vehicle ID.")
+    else:
+        print("Enter a valid Vehicle ID.") # If vehicle ID not found
         print("")
         rent_vehicle(Customer)
 
-
+# Function to return a rented vehicle
 def return_vehicle(Customer):
-    rent_ID = int(input("Enter the vehicle ID of the vehicle you want to return: "))
+    rent_ID = int(input("Enter the vehicle ID of the vehicle you want to return: ")) # Input vehicle ID
     for x in Customer.rental_history:
         if rent_ID == x.vehicle_id:
             if rent_ID <= len(Vehicles):
-                current_vehicle = Vehicles[rent_ID-1]
+                current_vehicle = Vehicles[rent_ID - 1]
                 if current_vehicle.availability == False:
-                    print(f"Thanks for returning the vehicle")
+                    print(f"Thanks for returning the vehicle") # If vehicle returned successfully
                     current_vehicle.availability = True
                 for x in Customer.rental_history:
                     if x.vehicle_id == rent_ID:
                         x.returned = True
                 print("")
                 return
-    
-    print("You have not rented this Vehicle.")
+    print("You have not rented this Vehicle.") # If you try to return a vehicle not rented by you
     print("")
 
+# Function to print details of all vehicles
 def print_vehicles():
     for x in Vehicles:
         Vehicle.print_vehicle_details(x)
         print("")
 
-
-# ASK user number of filters, name of filters one by one, sort by, make filters as a list of filters, even when only one filter given
-def filter_vehicles(Vehicles: list, filters: list, sort_by = "rental_rate"): # by default sort by rental rate if user doesnt specify
-    # function directly prints to console, directly call fn with given arguments in main program
-    found = [] 
+# Function to filter and sort vehicles based on criteria
+def filter_vehicles(Vehicles: list, filters: list, sort_by="rental_rate"):
+    # Filter and sort vehicles
+    found = []
     f = filters.pop(0)
     if f == "vehicle_id":
             vid = int(input("Enter vehicle id: "))
@@ -170,31 +185,34 @@ def filter_vehicles(Vehicles: list, filters: list, sort_by = "rental_rate"): # b
     if not Vehicles:
         print("No such vehicles found.")
     if sort_by == "rental_rate":
-        filters.sort(key = lambda v: v.rental_rate)
+        filters.sort(key=lambda v: v.rental_rate)
     elif sort_by == "year":
         filters.sort(key=lambda v: v.year)
     elif sort_by == "make":
-        filters.sort(key = lambda v: v.make)
+        filters.sort(key=lambda v: v.make)
     elif sort_by == "model":
-        filters.sort(key = lambda v: v.model)
+        filters.sort(key=lambda v: v.model)
     elif sort_by == "vehicle_id":
-        filters.sort(key = lambda v: v.vehicle_id)
+        filters.sort(key=lambda v: v.vehicle_id)
     elif sort_by == "availability":
-        filters.sort(key = lambda v: v.availability)
+        filters.sort(key=lambda v: v.availability)
     else:
-        print("Not a valid sorting criteria.")
+        print("Not a valid sorting criteria.") # If sorting criteria not found
     for v in found:
         v.print_vehicle_details()
         print("")
-    
-class RentalManager():
+
+# Define a class for managing rentals
+class RentalManager:
     print("")
-    print("*"*100)
+    print("*" * 100)
     print("")
     print("Welcome to Car rental service.")
     print("")
     cus_id = Customer.input_customer_details()
-    def __init__(self, Vehicles=[], Customers = []):
+
+    def __init__(self, Vehicles=[], Customers=[]):
+        # Initialize rental manager with vehicles and customers
         self.Vehicles = Vehicles
         self.Customers = Customers
 
@@ -203,7 +221,9 @@ class RentalManager():
             current_customer = x
             break
 
+    @staticmethod
     def add_vehicles():
+        # Function to add new vehicles to the list
         while True:
             user_input = input("Press N to enter new vehicle to list: ")
             if user_input == "N":
@@ -213,34 +233,43 @@ class RentalManager():
                 model = input("Model: ")
                 year = input("Year: ")
                 rental_rate = int(input("Rental rate: "))
-                luxury = (input("Luxary: "))
+                luxury = (input("Luxury: "))
                 if luxury == "Yes":
                     Vehicles.append(LuxuryVehicle(vehicle_id, make, model, year, rental_rate, True, True, True))
                 else:
                     Vehicles.append(Vehicle(vehicle_id, make, model, year, rental_rate, True))
-            else: 
+            else:
                 break
 
+    @staticmethod
     def remove_vehicles():
+        # Function to remove vehicles from the list
         user_input = input("Enter ID of vehicle to be removed: ")
         for x in Vehicles:
             if x.vehicle_id == user_input:
                 Vehicles.remove(x)
 
+    @staticmethod
     def generate_rental_report():
+        # Function to generate a report of all customer rentals
         print("")
         for x in Customers:
             print(f"Customer ID: {x.customer_id}, Customer Name: {x.name}")
             Customer.print_rental_history(x)
             print("")
 
-    while(True):
+    while True:
+        # Interactive loop for rental manager operations
         user_input = input("Please enter R for rent, Re for return, V for list of Vehicles, F to filter Vehicles, Q for quit, L for logout: ")
-        if user_input == "R": rent_vehicle(current_customer)
-        elif user_input == "Re": return_vehicle(current_customer)
-        elif user_input == "V": print_vehicles()
-        elif user_input == "F": 
-            print(f"Available filters are: ")
+        if user_input == "R":
+            rent_vehicle(current_customer)
+        elif user_input == "Re":
+            return_vehicle(current_customer)
+        elif user_input == "V":
+            print_vehicles()
+        elif user_input == "F":
+            # Ask for which filters to apply
+            print("Available filters are: ")
             print("vehicle_Id, make, model, rental_rate, year, availability")
             filters = int(input("How many filters do you want to apply: "))
             f_arr = []
@@ -251,17 +280,21 @@ class RentalManager():
             print("vehicle_id, make, model, rental_rate, year, availability")
             sby = input("Enter sorting criteria: ")
             filter_vehicles(Vehicles, f_arr, sby)
-        elif user_input == "A": add_vehicles()
-        elif user_input == "D": remove_vehicles()
-        elif user_input == "G": generate_rental_report()
-        elif user_input == "L": 
+        elif user_input == "A":
+            add_vehicles()
+        elif user_input == "D":
+            remove_vehicles()
+        elif user_input == "G":
+            generate_rental_report()
+        elif user_input == "L":
             print("Please login again or create a new ID.")
             cus_id = Customer.input_customer_details()
             for x in Customers:
                 if cus_id == x.customer_id:
                     current_customer = x
                     break
-        elif user_input == "Q": break
+        elif user_input == "Q":
+            break
 
-
+# Initialize the rental manager with the list of vehicles and customers
 RentalManager(Vehicles, Customers)
